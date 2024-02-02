b09884cfb152f6c10a339ac77377e44b4f565b92
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2024-02-02 12:35:15 +0100

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Custom for REDMI AX6000
+# Custom for REDMI AX6S
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
